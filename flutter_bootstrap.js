1d6670b93149d52c0811d57d534c6b7118c91371
katkuskris <katkuskris@users.noreply.github.com>
2025-08-10 20:34:25 +0000

    deploy: 6e76c6177e6ad31cbe13c9f87053b656bf4a261f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3370320071"
+    serviceWorkerVersion: "4077940945"
   }
 });
